9368c8852e28d94088c038f470570b74dc6b1fe6
shihanwj <shihanwei495@gmail.com>
2024-10-03 09:39:04 +0200

    Delete HelloWorld directory

--- HelloWorld/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module es.ieslosmontecillos.helloworld {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens es.ieslosmontecillos.helloworld to javafx.fxml;
-    exports es.ieslosmontecillos.helloworld;
-}
